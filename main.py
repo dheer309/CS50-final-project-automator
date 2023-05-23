db19921b28bb3dbda938f573fc02c618dc3eacd9
pritansh sahsani <82302289+pritansh-sahsani@users.noreply.github.com>
2023-05-23 13:22:14 +0530

    Removed non-essential utilities and added new utilities

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from PIL import Image, ImageFilter
 import os
 import shutil
 import feedparser
+from spellchecker import SpellChecker
 
+from textblob import TextBlob
 from nltk.corpus import wordnet
 
 from sumy.parsers.plaintext import PlaintextParser
@@ -22,39 +24,6 @@ def get_top_stories():
 def present_in_bulleted_list(stories):
     for story in stories:
         print(f'• {story}')
-
-def resize_images(input_dir, output_dir, size):
-    for filename in os.listdir(input_dir):
-        if filename.endswith('.jpg') or filename.endswith('.png'):
-            image_path = os.path.join(input_dir, filename)
-            image = Image.open(image_path)
-            resized_image = image.resize(size)
-            output_path = os.path.join(output_dir, f"resized_{filename}")
-            resized_image.save(output_path)
-            print(f'Resized {filename} successfully. Renamed to resized_{filename}')
-
-
-def crop_images(input_dir, output_dir, crop_area):
-    for filename in os.listdir(input_dir):
-        if filename.endswith('.jpg') or filename.endswith('.png'):
-            image_path = os.path.join(input_dir, filename)
-            image = Image.open(image_path)
-            cropped_image = image.crop(crop_area)
-            output_path = os.path.join(output_dir, f"cropped_{filename}")
-            cropped_image.save(output_path)
-            print(f'Cropped {filename} successfully. Renamed to cropped_{filename}')
-
-
-def apply_blur(input_dir, output_dir, filter_type):
-    for filename in os.listdir(input_dir):
-        if filename.endswith('.jpg') or filename.endswith('.png'):
-            image_path = os.path.join(input_dir, filename)
-            image = Image.open(image_path)
-            filtered_image = image.filter(filter_type)
-            filter_name = filter_type.__name__.lower()
-            output_path = os.path.join(output_dir, f"{filter_name}_{filename}")
-            filtered_image.save(output_path)
-            print(f'Applied {filter_type} filter to {filename} successfully. Renamed to {filter_name}_{filename}')
 
 def organize_files(source_dir, target_dir):
     for filename in os.listdir(source_dir):
@@ -80,49 +49,58 @@ def get_synonyms(word):
     synonyms = []
     for syn in wordnet.synsets(word):
         for lemma in syn.lemmas():
-            synonym = lemma.name()
-            if synonym != word:
-                synonyms.append(synonym)
+            synonyms.append(lemma.name())
     return synonyms
+
+def replace_with_synonyms(text):
+    tokens = word_tokenize(text)
+    replaced_tokens = []
+    for token in tokens:
+        synonyms = get_synonyms(token)
+        if synonyms:
+            replaced_tokens.append(synonyms[0])  # Replace with the first synonym
+        else:
+            replaced_tokens.append(token)
+    return ' '.join(replaced_tokens)
+
+def get_sentiment_score(text):
+    blob = TextBlob(text)
+    sentiment_score = blob.sentiment.polarity
+    return sentiment_score
+
+def spell_check(text):
+    spell = SpellChecker()
+    corrected_text = ""
+    words = text.split()
+
+    for word in words:
+        corrected_word = spell.correction(word)
+        corrected_text += corrected_word + " "
+
+    return corrected_text.strip()
 
 def main():
     parser = argparse.ArgumentParser(description='Automation Tools')
     parser.add_argument('--summarizenews', action='store_true', help='Summarize top stories from Google News')
-    parser.add_argument('--resize', action='store_true', help='Resize images')
-    parser.add_argument('--crop', action='store_true', help='Crop images')
-    parser.add_argument('--resize-size', type=int, nargs=2, metavar=('WIDTH', 'HEIGHT'), help='Size for image resizing')
-    parser.add_argument('--crop-area', type=int, nargs=4, metavar=('LEFT', 'UP', 'RIGHT', 'DOWN'), help='Crop area for images')
-    parser.add_argument('--blur', action='store_true', help='Apply blur effect to images')
-    parser.add_argument('--organize', action='store_true', help='Organize files')
-    parser.add_argument('--input-dir', type=str, help='Input directory path')
-    parser.add_argument('--output-dir', type=str, help='Output directory path')
-    parser.add_argument('--summarizetext', type=str, help='Text to summarize')
-    parser.add_argument('--num_sentences', type=int, default=3, help='Number of sentences in the summary')
-    parser.add_argument('--thesaurus', metavar='WORD', help='Find synonyms for the specified word')
+
+    parser.add_argument('--organize', action='store_true', help='Organize files into sub folders.')
+    parser.add_argument('--input-dir', type=str, help='Input directory path.')
+    parser.add_argument('--output-dir', type=str, help='Output directory path.')
+    
+    parser.add_argument('--summarizetext', type=str, help='Text to summarize.')
+    parser.add_argument('--num_sentences', type=int, default=3, help='Number of sentences in the summary.')
+    
+    parser.add_argument('--text-spin', type=str, help='Replace words in your text with synonyms.')
+
+    parser.add_argument('--sentiment-analysis', type=str, help='Analyses text to tell if its positive negitive or neutral.')
+
+    parser.add_argument('--spell-checker', type=str, help='Corrects the inncorrect spellings in the text.')
 
     args = parser.parse_args()
 
     if args.summarizenews:
         stories = get_top_stories()
         present_in_bulleted_list(stories)
-
-    if args.resize and args.input_dir and args.output_dir:
-        input_directory = args.input_dir
-        output_directory = args.output_dir
-        size = tuple(args.resize_size)
-        resize_images(input_directory, output_directory, size)
-
-    if args.crop and args.input_dir and args.output_dir:
-        input_directory = args.input_dir
-        output_directory = args.output_dir
-        crop_area = tuple(args.crop_area)
-        crop_images(input_directory, output_directory, crop_area)
-
-    if args.blur and args.input_dir and args.output_dir:
-        input_directory = args.input_dir
-        output_directory = args.output_dir
-        filter_type = ImageFilter.BLUR
-        apply_blur(input_directory, output_directory, filter_type)
 
     if args.organize and args.input_dir and args.output_dir:
         source_directory = args.input_dir
@@ -135,15 +113,18 @@ def main():
         for sentence in summary_sentences:
             print("- " + sentence)
     
-    if args.thesaurus:
-        word = args.thesaurus
-        synonyms = get_synonyms(word)
-        if synonyms:
-            print(f"Synonyms of '{word}':")
-            for synonym in synonyms:
-                print(synonym)
-        else:
-            print(f"No synonyms found for '{word}'")
+    if args.text-spin:
+        new_text = replace_with_synonyms(args.text-spin)
+        print("New text with synonyms:")
+        print(new_text)
+
+    if args.sentiment-analysis:
+        sentiment_score = get_sentiment_score(args.sentiment-analysis)
+        print("Sentiment score: ", sentiment_score)
+
+    if args.spell-checker:
+        corrected_text = spell_check(args.spell-checker)
+        print("Corrected text: ", corrected_text)
 
 if __name__ == '__main__':
     main()
